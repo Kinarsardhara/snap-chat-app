ff6d24e028d3f5e6d0cfadb1fd4bb3c60358c5f9
kinar sardhara <125781461+Kinarsardhara@users.noreply.github.com>
2024-01-27 09:08:55 -0500

    snap-chat-app

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
+
+const connectToMongo = async ()  => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URL,
+      {}
+    );
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+    db.once("open", () => {
+      console.log("Connected to MongoDB");
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+connectToMongo();
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on Port,${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "https://snap-chat-app.netlify.app",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.messages);
+    }
+  });
+});
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const { register, login, setAvatar, getAllUser, test } = require("../controllers/userControllers");
+const router = require("express").Router();
+
+router.post("/test",test);
+router.post("/register",register);
+router.post("/login",login);
+router.post("/setAvatar/:id",setAvatar);
+router.get("/allusers/:id",getAllUser)
+module.exports = router;
